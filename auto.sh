40d03dd252b511da278c6ef5aaa89f0b854bd9fb
Andrew Martin <MRProproduction1@gmail.com>
2016-04-23 23:40:05 +0100

    Added files via upload

---
+++ auto.sh
@@ -0,0 +1,18 @@
+## Auto C# compile by Scatman John ## 
+#enter script name with .cs at the end#
+Scripts_name="hello.cs"
+#enter path were you would like your .exe build to make move to
+path="/var"
+#name of your file is called when makeing a tweet#
+name="helloworld"
+
+#Do not edit past this point#
+
+#This command compiles your code#
+dmcs $Scripts_name
+#this command make a copy of your compiled program and moves to folder for you#
+cp $Scripts_name $path 
+#this command removed the left over .exe#
+rm $Scripts_name
+twidge update "New Build of $name has been made and is able to be downloaded new build every 3h"
+
